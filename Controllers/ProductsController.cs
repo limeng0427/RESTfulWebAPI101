85ed083b9c5c69413d0dc1e0d30602a6b907065e
Liam Li <limeng0427@gmail.com>
2019-09-22 23:00:18 +1200

    Add Meaningful Status Return

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -14,47 +14,61 @@ namespace WebServer.Controllers
         [HttpGet] // Action Route
         //[Route("all")] // optional Action Route which only works on https://5001/api/products/all
         //[HttpGet("all")]
-        public Product[] GetAllProducts(){ // Action Name does not affect working logic, so have a self-descriptive name
-            return FakeData.Products.Values.ToArray();
+        public ActionResult GetAllProducts(){ // Action Name does not affect working logic, so have a self-descriptive name
+            if(FakeData.Products != null){
+                return Ok(FakeData.Products.Values.ToArray());//200 return meaningfull status code
+            } else {
+                return NotFound();//404
+            }
         }
 
         [HttpGet("{id}")]
         //[HttpGet("ByID/{id}")]// optional way to make it more self-descriptive, works on https://5001/api/products/byid/0
-        public Product Get(int id){
+        public ActionResult Get(int id){
             if(FakeData.Products.ContainsKey(id))
-                return FakeData.Products[id];
+                return Ok(FakeData.Products[id]);
             else
-                return null;
+                return NotFound();
         }
 
         [HttpGet("from/{low:int}/to/{high:int}")]
         // example: https://5001/api/products/from/0/to/100
-        public Product[] Get(int low, int high){
+        public ActionResult Get(int low, int high){
             var products = FakeData.Products.Values.Where(p=>p.Price >= low && p.Price <= high).ToArray();
-            return products;
+            if(products.Any())
+                return Ok(products);
+            else
+                return NotFound();
         }
 
         [HttpPost]
-        public Product Post([FromBody] Product product){
+        public ActionResult Post([FromBody] Product product){
             product.ID = FakeData.Products.Keys.Max()+1;
             FakeData.Products.Add(product.ID,product);
-            return product;
+            return Created($"api/products/{product.ID}",product);//url+new object 201
+            //Hypermedia as the engine of application state HATEOAS
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Product product){
+        public ActionResult Put(int id, [FromBody] Product product){
             if(FakeData.Products.ContainsKey(id)){
                 var target = FakeData.Products[id];
                 target.ID = product.ID;
                 target.Name = product.Name;
                 target.Price = product.Price;
+                return Ok();
+            } else{
+                return NotFound();
             }
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id){
+        public ActionResult Delete(int id){
             if(FakeData.Products.ContainsKey(id)){
                 FakeData.Products.Remove(id);
+                return Ok();
+            } else{
+                return NotFound();
             }
         }
     }
